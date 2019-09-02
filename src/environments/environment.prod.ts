e84317490cbc5d6d5c4b9e5d6c6a241bba0a8a1c
hugogomess <hugogomes02@gmail.com>
2019-09-02 02:56:17 -0300

    add apiUrl in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  debugMode: false,
+  apiUrl: 'https://api-ufca-sgd.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import 'zone.js/dist/zone-error';
+
 export const environment = {
-  production: false
+  production: false,
+  debugMode: true,
+  apiUrl: 'https://api-ufca-sgd.herokuapp.com'
 };
 
 /*
