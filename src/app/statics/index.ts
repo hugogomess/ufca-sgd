db721f4519b6ba0bfb0821596f3899347c9a5d0d
hugogomess <hugogomes02@gmail.com>
2019-09-13 15:04:16 -0300

    add home component

--- src/app/statics/index.ts
+++ src/app/statics/index.ts
@@ -1,2 +1,2 @@
 export * from './statics.module';
-
+export * from './statics-routing.module';
--- src/app/statics/statics.module.ts
+++ src/app/statics/statics.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header';
 import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './components';
+import { HomeComponent } from './components';
 
 
 @NgModule({
-declarations: [HeaderComponent, ],
+declarations: [HeaderComponent, HomeComponent, ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
     HeaderComponent,
+    HomeComponent
   ]
 })
 export class StaticsModule { }
