eef11d6004932ba418b542f3a39d986ae2f6f6a5
hugogomess <hugogomes02@gmail.com>
2019-09-21 03:06:04 -0300

    add login e2e test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element(by.css('UFCA - Sistema de Gestão de Demandas')).getText() as Promise<string>;
   }
 }
