055874790ee0b52cdfeaaa8de68929dc42feaf93
Evandro.J <juniok591@gmai.com>
2019-10-03 11:53:10 -0300

    Server-error

--- src/app/statics/statics.module.ts
+++ src/app/statics/statics.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components';
 import { HomeComponent, NotFoundComponent } from './components';
 import { PermissionDenidComponent } from './components/permission-denid/permission-denid.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
 
 
 
 @NgModule({
-declarations: [HeaderComponent, HomeComponent, NotFoundComponent, PermissionDenidComponent, ],
+declarations: [HeaderComponent, HomeComponent, NotFoundComponent, PermissionDenidComponent, ServerErrorComponent, ],
   imports: [
     CommonModule,
     RouterModule
