4818f22f59a4c8d64f4ed590bffee0cb3c5cd3e8
Evandro.J <juniok591@gmai.com>
2019-11-26 11:55:11 -0300

    router navigate added

--- src/app/demand/demand-routing.module.ts
+++ src/app/demand/demand-routing.module.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 
 import { ListDemandsComponent, DemandPageComponent } from './components';
 import { HomeComponent } from './components';
+import { AuthGuard } from '../guards';
 
 export const DemandRoutes: Routes = [
-    {path: 'admin/demandas', component: ListDemandsComponent},
+    {path: 'admin/demandas', component: ListDemandsComponent,canActivate: [AuthGuard]},
     {path: '', component: HomeComponent},
     {path: 'demandas/:demandId', component: DemandPageComponent}
 ];
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,5 +4,5 @@ import { ShowUsersComponent } from './components';
 import { AuthGuard } from '../guards';
 
 export const UserRoutes: Routes = [
-    {path: 'admin/usuarios', component: ShowUsersComponent}
+    {path: 'admin/usuarios', component: ShowUsersComponent,canActivate: [AuthGuard]}
 ];
